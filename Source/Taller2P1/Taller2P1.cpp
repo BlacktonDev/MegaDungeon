c80aa1711a4b7791a20ed2b74de16525ac789e52
Romo <jantonio.romot@gmail.com>
2024-03-11 11:01:59 -0600

    setup
    setup
    - C++
    - Maximun
    - Starter Content

---
+++ Source/Taller2P1/Taller2P1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Taller2P1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Taller2P1, "Taller2P1" );
