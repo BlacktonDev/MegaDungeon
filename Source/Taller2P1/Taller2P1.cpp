47ab18ec9807f77deaaee3b31bc653c1f95b7823
Romo <jantonio.romot@gmail.com>
2024-03-11 18:05:29 -0600

    Revert "setup"
    This reverts commit c80aa1711a4b7791a20ed2b74de16525ac789e52.

--- Source/Taller2P1/Taller2P1.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Taller2P1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Taller2P1, "Taller2P1" );
