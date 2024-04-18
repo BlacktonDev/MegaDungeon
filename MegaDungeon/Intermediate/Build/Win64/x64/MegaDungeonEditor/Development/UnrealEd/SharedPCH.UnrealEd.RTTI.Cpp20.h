899a38b8cb2db1c76403bcc83c1af7367785625b
kumorigrr <jennifer.nicole.rizo@gmail.com>
2024-04-18 12:22:14 -0700

    Import de ARTE
    Solo son los trabajos que hemos hecho y cree unos niveles para cada uno

--- MegaDungeon/Intermediate/Build/Win64/x64/MegaDungeonEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h
+++ MegaDungeon/Intermediate/Build/Win64/x64/MegaDungeonEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "C:/Users/black/Documents/UNIAT/6_Semestre/TallerVideojuegosII/Taller2P1/MegaDungeon/Intermediate/Build/Win64/x64/MegaDungeonEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.RTTI.Cpp20.h"
+#include "D:/6toSemestre/4_Taller_VJ_II/P1/Taller2P1/MegaDungeon/Intermediate/Build/Win64/x64/MegaDungeonEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.RTTI.Cpp20.h"
 #include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
